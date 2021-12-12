8128ee67343041cbdd7808b9e59c2b446ed95408
Andriy84 <79792876+Andriy84@users.noreply.github.com>
2021-12-12 15:50:11 +0200

    Add files via upload

---
+++ Підготовка 2/z1.js
@@ -0,0 +1,13 @@
+let zag = document.getElementById(`zagolovok`);
+let danni = `<h1>Всім привіт!</h1>`
+for(i=0; i<9;i++){
+danni += `<h1>Всім привіт!</h1>`
+} 
+zag.innerHTML = danni;
+ 
+let abz = document.getElementById(`abzac`);
+let abzaci = `<p align ="right">дивись на дошку!</p>`
+for(i=0; i<14;i++){ 
+    abzaci += `<p align ="right">дивись на дошку!</p>`
+}
+abz.innerHTML = abzaci;
